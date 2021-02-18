42d981e11f4d0efa1400256f9dde1fcd0ea370cb
dropdbassett <60153627+dropdbassett@users.noreply.github.com>
2021-02-18 18:47:19 +0000

    Add colloquial.js
    List of colloquial terms for each goal.

---
+++ colloquial.js
@@ -0,0 +1,174 @@
+var bingoGenerator = require("./generators/generator_bases/srl_generator_v5.js");
+
+var thisVersion = "v3 beta 9";
+
+var bingoList = [];
+bingoList[1] = [
+{ name: "11 Delfino Blues"},
+{ name: "15 Delfino Blues"},
+{ name: "5 Linkeds"},
+{ name: "3 Bosses"},
+{ name: "3 Shines from Blue Trades"}
+];
+bingoList[2] = [
+{ name: "Two 100s"},
+{ name: "50 from 2 OR 150 from Delfino"},
+{ name: "Bianco 4 Fluddless"},
+{ name: "Ricco 3 Hoverless"},
+{ name: "Pianta 100s"}
+];
+bingoList[3] = [
+{ name: "18 Lives"},
+{ name: "20 Lives"},
+{ name: "Ricco 100s"},
+{ name: "3 Reds"},
+{ name: "Hidden in Bianco"}
+];
+bingoList[4] = [
+{ name: "15 Bianco Blues"},
+{ name: "15 Ms"},
+{ name: "17 Ms"},
+{ name: "Hidden Shine in Ricco"},
+{ name: "Pianta 3 Damageless"}
+];
+bingoList[5] = [
+{ name: "3 Fruit Ladies"},
+{ name: "5 NPCs"},
+{ name: "15 Gelato Blues"},
+{ name: "4 from Sirena"},
+{ name: "3 Secrets"}
+];
+bingoList[6] = [
+{ name: "1 Turbo"},
+{ name: "3 Hiddens"},
+{ name: "9 Sirena Blues"},
+{ name: "2 Hidden Reds Hoverless"},
+{ name: "2 Hiddens in Gelato"}
+];
+bingoList[7] = [
+{ name: "Boss Rematch"},
+{ name: "6 from Ricco"},
+{ name: "2 Hiddens 1 Level (NO Gelato)"},
+{ name: "Sirena 4R"},
+{ name: "Bianco 100s"}
+];
+bingoList[8] = [
+{ name: "16 Pianta Blues"},
+{ name: "2 Rockets"},
+{ name: "4 Unique Bosses"},
+{ name: "4 from Pianta"},
+{ name: "Gelato 100s"}
+];
+bingoList[9] = [
+{ name: "5 Enemies"},
+{ name: "4 Reds"},
+{ name: "23 Ricco Blues"},
+{ name: "5 Birds"},
+{ name: "16 Pinna Blues"}
+];
+bingoList[10] = [
+{ name: "4 Bosses"},
+{ name: "7 Enemies"},
+{ name: "Wiggler"},
+{ name: "20 Bianco Blues"},
+{ name: "Sirena 100s"}
+];
+bingoList[11] = [
+{ name: "8 Delfinos"},
+{ name: "9 Delfinos"},
+{ name: "Hidden in Sirena"},
+{ name: "4 Secrets"},
+{ name: "7 Linkeds"}
+];
+bingoList[12] = [
+{ name: "Hidden in Pinna"},
+{ name: "7 Pinna Beach Blues OR 4 Fruit Ladies"},
+{ name: "20 from 2"},
+{ name: "4 Hiddens"},
+{ name: "5 Bosses"}
+];
+bingoList[13] = [
+{ name: "6 from Pinna"},
+{ name: "Pinna 5 from the back Hoverless"},
+{ name: "Pinna Shadow Mario"},
+{ name: "Hidden in Pianta"},
+{ name: "6 Pipes"},
+{ name: "9 Pipes"}
+];
+bingoList[14] = [
+{ name: "Bells OR Lighthouse"},
+{ name: "Lily Pad NO Fruit Clipping"},
+{ name: "Ride Yoshi in 2 Worlds"},
+{ name: "All Pinna Yellow Button Coins"},
+{ name: "5 Reds"}
+];
+bingoList[15] = [
+{ name: "12 Noki Blues"},
+{ name: "Pinna 100s"},
+{ name: "100 in a Secret"},
+{ name: "50 from 3 OR 100s from a Boss Shine"}
+];
+bingoList[16] = [
+{ name: "7 Birds"},
+{ name: "1 Butterfly (No Pinna)"},
+{ name: "4 from Gelato"},
+{ name: "16 Noki Blues OR 5 from Gelato"},
+{ name: "3 Rockets"}
+];
+bingoList[17] = [
+{ name: "5 Secrets"},
+{ name: "5 from 2"},
+{ name: "5 from Bianco"},
+{ name: "5 Hiddens"},
+{ name: "4 from Noki"}
+];
+bingoList[18] = [
+{ name: "Gold Birds"},
+{ name: "1 from each"},
+{ name: "2 from 5"},
+{ name: "Moles"}
+];
+bingoList[19] = [
+{ name: "6 Reds"},
+{ name: "Noki 100s"},
+{ name: "18 Sirena Blues"},
+{ name: "Delfino Rocket"},
+{ name: "26 Shines"}
+];
+bingoList[20] = [
+{ name: "Eel"},
+{ name: "Three 100s"},
+{ name: "6 Hiddens"},
+{ name: "7 in Secrets"}
+];
+bingoList[21] = [
+{ name: "2 Turbos"},
+{ name: "12 Level Shines"},
+{ name: "12 from Delfino (With Trades)"},
+{ name: "15 from Delfino (With Trades)"},
+{ name: "80 Totals"}
+];
+bingoList[22] = [
+{ name: "3 from 4"},
+{ name: "4 from 3"},
+{ name: "2 from 7"},
+{ name: "8 Episodes"},
+{ name: "8 from 1"}
+];
+bingoList[23] = [
+{ name: "8 NPCs OR Ricco 8"},
+{ name: "1 Shadow Mario"},
+{ name: "Pianta Shadow Mario"},
+{ name: "2 Piantissimos"},
+{ name: "All Sand Bird Blues OR All Eel Blues"}
+];
+bingoList[24] = [
+{ name: "40 Totals"},
+{ name: "45 Totals"},
+{ name: "50 Totals"}
+];
+bingoList[25] = [
+{ name: "60 Totals"},
+{ name: "65 Totals"},
+{ name: "70 Totals"}
+];
